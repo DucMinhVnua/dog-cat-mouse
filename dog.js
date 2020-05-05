f8b18361daa7c869b61574a85b71378ad2402aa2
minh <ndminhvnua@gmai.com>
2020-05-05 13:54:07 +0700

    name for dog

--- dog.js
+++ dog.js
@@ -1,7 +1,17 @@
-function dog() {
+var chalk = require('chalk');
+
+function dog(name) {
+	this.name = name;
 	this.stomach = [];
 }
 dog.prototype.eat = function(cat) {
 	// body...
-	this.stomach.push(cat);
+	this.stomach.push(cat); 
 };
+
+dog.prototype.sayHi = function() {
+	// body...
+	console.log('Hi! I am a dog. My name is ' + chalk.red(this.name));
+};
+
+module.exports = dog;
---
+++ index.js
@@ -0,0 +1,3 @@
+var dog = require('./dog');
+var dog1 = new dog('Tom');
+dog1.sayHi();
